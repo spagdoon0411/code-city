847efd804149ae20910b984cfd21027ab0035554
tommychobo <thomaschoboter@gmail.com>
2024-02-01 12:35:40 -0800

    File parsing stuff

--- FileParser.java
+++ FileParser.java
@@ -5,10 +5,60 @@ import java.util.ArrayList;
 
 public class FileParser{
     private File file;
+    private String fStr;
     private ArrayList<DeclaredPart> ooParts;
     public FileParser(String path){
         file = new File(path);
+        try{
+            Scanner sc = new Scanner(file);
+            fStr = "";
+            while(sc.hasNextLine()){
+                fStr.concat(sc.nextLine()+"\n");
+            }
+            sc.close();
+        }catch(FileNotFoundException e){
+            System.err.println("I literally have no idea where that file is");
+        }
+        removeStrComments();
         ooParts = new ArrayList<DeclaredPart>();
+    }
+
+
+    /**
+     * just gets rid of all of the parts of fStr that should be commented out.
+     * This is called by the constructor and that's why it's set to private.
+     * Now we can just search through fStr for all of our checks and we don't
+     * have to worry about comments getting in the way
+     */
+    private void removeStrComments(){
+        int index = 0;
+        int lastCommentStartInd = 0;
+        while(index < fStr.length()-1){
+            String indicator = fStr.substring(index, index+2);
+            //check for single line comments and remove them
+            if(indicator.equals("//")){
+                lastCommentStartInd = index;
+                while(index < fStr.length() && fStr.charAt(index) != '\n'){
+                    index++;
+                }
+                index++;
+                fStr = fStr.substring(0, lastCommentStartInd)+fStr.substring(index, fStr.length());
+                index -= (index - lastCommentStartInd);
+            }
+            //check for multiline comments and remove them
+            if(indicator.equals("/*")){
+                lastCommentStartInd = index;
+                String endIndicator = "";
+                while(index < fStr.length()-1 && "*/".equals(endIndicator)){
+                    endIndicator = fStr.substring(index, index+2);
+                    index++;
+                }
+                index += 2;
+                fStr = fStr.substring(0, lastCommentStartInd)+fStr.substring(index, fStr.length());
+                index -= (index - lastCommentStartInd);
+            }
+            index++;
+        }
     }
 
     /**
@@ -52,6 +102,33 @@ public class FileParser{
         try{
             Scanner fs = new Scanner(file);
             //The package assignment should always be the first line of the file
+            packageLine = fs.nextLine();
+            if(packageLine.startsWith("package")){
+                int stopInd = packageLine.indexOf(';');
+                packageLine = packageLine.substring(8, stopInd);
+            }else{
+                fs.close();
+                return null;
+            }
+            fs.close();
+        }catch(FileNotFoundException e){
+            System.err.println("Can't find file.");
+            e.printStackTrace();
+        }
+        return packageLine;
+    }
+
+    /**
+     * Gets the package of a java file. If there is no package present,
+     * returns null.
+     * @return the package name as a string
+     */
+    public String getClassName(){
+        String packageLine = null;
+        try{
+            Scanner fs = new Scanner(file);
+            //Looking for the first occurrence of " class "
+
             packageLine = fs.nextLine();
             if(packageLine.startsWith("package")){
                 int stopInd = packageLine.indexOf(';');
