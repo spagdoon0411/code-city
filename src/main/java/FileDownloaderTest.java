c6c71de75e9d169fd5fc06f3551e179909e607a9
spagdoon0411 <spandan.suthar20042011@gmail.com>
2024-02-06 20:12:57 -0800

    add file parser to complete preprocessing pipeline

--- src/main/java/FileDownloaderTest.java
+++ src/main/java/FileDownloaderTest.java
@@ -1,5 +1,5 @@
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 import org.junit.jupiter.api.io.TempDir;
 
 import java.io.File;
---
+++ src/main/java/Main.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import java.util.*;
+import java.util.List;
+
+public class Main {
+
+    final static int FRAME_WIDTH = 800;
+    final static int FRAME_HEIGHT = 600;
+
+    final static int CANVAS_WIDTH = 9000;
+
+    final static int CANVAS_HEIGHT = 9000;
+
+    public static void main(String[] args)
+    {
+
+        // String recommendedTest = "https://github.com/spagdoon0411/code-city-test-remote";
+        String remoteUrl = JOptionPane.showInputDialog("GitHub repository:");
+
+        String downloadPath = JOptionPane.showInputDialog("Absolute download path:");
+
+        // FileDownloader fd = new FileDownloader(remoteUrl, downloadPath);
+        FileDownloader fd = new FileDownloader(remoteUrl, downloadPath);
+        fd.downloadFiles();
+
+        FileParser parser = new FileParser(downloadPath);
+        List<FileInfo> fileInfos = parser.getFileInfos();
+
+        Map<String, List<Building>> buildings = FilesToBuildings.processFiles(fileInfos);
+
+        View v = new View(
+                "Code City",
+                new BuildingRenderer(buildings),
+                FRAME_WIDTH,
+                FRAME_HEIGHT,
+                CANVAS_WIDTH,
+                CANVAS_HEIGHT
+        );
+
+        v.produce();
+    }
+}
